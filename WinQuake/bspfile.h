7812394320e0339f61d708bc509ae3570a4b51c2
mrcatacroquer <manulucio@gmail.com>
2015-10-07 16:33:50 +0200

    Change from GH

--- WinQuake/bspfile.h
+++ WinQuake/bspfile.h
@@ -1,6 +1,7 @@
 /*
 Copyright (C) 1996-1997 Id Software, Inc.
 
+Change in Github
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
