5754be5e223ab8c3054137c9602cf72b6d1cf9ff
mlucio <mlucio@.(none)>
2015-10-07 16:27:29 +0200

    mlucio||10/7/2015 4:26:26 PM||/main :: This is a change from Plastic SCM.

--- WinQuake/anorm_dots.h
+++ WinQuake/anorm_dots.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 1996-1997 Id Software, Inc.
+Copyright (C) 2000-2015 Id Software, Inc.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
